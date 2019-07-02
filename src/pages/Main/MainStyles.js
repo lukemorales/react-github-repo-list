2c18913204ec05c4cc22b52c7249948dfbbefcf2
Luke Morales <lukemorales@live.com>
2019-07-02 01:06:07 -0300

    chore: Styled Main Page

--- src/pages/Main/MainStyles.js
+++ src/pages/Main/MainStyles.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #fff;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
