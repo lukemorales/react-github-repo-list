dad0a03eceea7a5a0a88c4f799075ba1a47d56b5
Luke Morales <lukemorales@live.com>
2019-07-02 02:35:21 -0300

    feat: Fetch Repo Info and Open Issues from GitHub

--- src/pages/Repository/Repository.js
+++ src/pages/Repository/Repository.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 // import {} from './RepositoryStyles.js';
 
-export default function Repository({ match }) {
-  return (
-    <>
-      <h1>Repository: {decodeURIComponent(match.params.repo)}</h1>
-    </>
-  );
+export default class Repository extends Component {
+  state = {
+    repo: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repo);
+
+    const [repo, issues] = await Promise.all([
+      await api.get(`/repos/${repoName}`),
+      await api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repo: repo.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repo, issues, loading } = this.state;
+    return (
+      <>
+        <h1>Repository: </h1>
+      </>
+    );
+  }
 }
